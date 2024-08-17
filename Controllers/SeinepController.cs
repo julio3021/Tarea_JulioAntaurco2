6ac83ec2e679b98a70a2e43c8edc54dc4b9674f6
Julio3021 <julio_antaurco@usmp.pe>
2024-08-17 15:54:45 -0500

    Primer commit

---
+++ Controllers/SeinepController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Tarea_JulioAntaurco2.Controllers
+{
+
+    public class SeinepController : Controller
+    {
+        private readonly ILogger<SeinepController> _logger;
+
+        public SeinepController(ILogger<SeinepController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Controllers/TobyController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Tarea_JulioAntaurco2.Controllers
+{
+
+    public class TobyController : Controller
+    {
+        private readonly ILogger<TobyController> _logger;
+
+        public TobyController(ILogger<TobyController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
